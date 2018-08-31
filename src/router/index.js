2835b3d3619f39ab10d55769a660802209ec4ef6
jerry.zheng <jerry.zheng@cloudkeeper.cn>
2018-08-31 18:39:58 +0800

    路由图标修改

--- src/router/index.js
+++ src/router/index.js
@@ -42,132 +42,132 @@ export const asyncMap = [{
     path: '/test',
     name: 'test',
     component: Layout,
-    meta: { title: '测试1', icon: 'el-icon-success', cache: true, menu: true },
+    meta: { title: '测试1', icon: 'fa fa-windows', cache: true, menu: true },
     children: [{
         path: 'a',
         component: () => import('@/components/test-child'),
         name: 'test-child1',
-        meta: { title: '测试-子页面1', icon: 'el-icon-success', cache: true, menu: true }
+        meta: { title: '测试-子页面1', icon: 'fa fa-windows', cache: true, menu: true }
     }, {
         path: 'b',
         component: () => import('@/components/test-child2'),
         name: 'test-child2',
-        meta: { title: '测试-子页面2', icon: 'el-icon-success', cache: true, menu: true }
+        meta: { title: '测试-子页面2', icon: 'fa fa-windows', cache: true, menu: true }
     }, {
         path: 'c',
         component: () => import('@/components/test-child2'),
         name: 'test-child3',
-        meta: { title: '测试-子页面3', icon: 'el-icon-success', cache: true, menu: false }
+        meta: { title: '测试-子页面3', icon: 'fa fa-windows', cache: true, menu: false }
     }]
 }, {
     path: '/test2',
     name: 'test2',
     component: Layout,
-    meta: { title: '测试2', icon: 'el-icon-success', cache: true, menu: true },
+    meta: { title: '测试2', icon: 'fa fa-windows', cache: true, menu: true },
     children: [{
         path: 'a',
         component: () => import('@/components/test-child'),
         name: 'test-child4',
-        meta: { title: '测试-子页面4', icon: 'el-icon-success', cache: true, menu: true }
+        meta: { title: '测试-子页面4', icon: 'fa fa-windows', cache: true, menu: true }
     }, {
         path: 'b',
         component: () => import('@/components/test-child2'),
         name: 'test-child5',
-        meta: { title: '测试-子页面5', icon: 'el-icon-success', cache: true, menu: false }
+        meta: { title: '测试-子页面5', icon: 'fa fa-windows', cache: true, menu: false }
     }, {
         path: 'c',
         component: () => import('@/components/test-child2'),
         name: 'test-child6',
-        meta: { title: '测试-子页面6', icon: 'el-icon-success', cache: true, menu: true }
+        meta: { title: '测试-子页面6', icon: 'fa fa-windows', cache: true, menu: true }
     }]
 }, {
     path: '/test3',
     name: 'test3',
     component: Layout,
-    meta: { title: '测试3', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试3', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test4',
     name: 'test4',
     component: Layout,
-    meta: { title: '测试4', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试4', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test5',
     name: 'test5',
     component: Layout,
-    meta: { title: '测试5', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试5', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test6',
     name: 'test6',
     component: Layout,
-    meta: { title: '测试6', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试6', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test7',
     name: 'test7',
     component: Layout,
-    meta: { title: '测试7', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试7', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test8',
     name: 'test8',
     component: Layout,
-    meta: { title: '测试8', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试8', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test9',
     name: 'test9',
     component: Layout,
-    meta: { title: '测试9', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试9', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test10',
     name: 'test10',
     component: Layout,
-    meta: { title: '测试10', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试10', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test11',
     name: 'test11',
     component: Layout,
-    meta: { title: '测试11', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试11', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test12',
     name: 'test12',
     component: Layout,
-    meta: { title: '测试12', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试12', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test13',
     name: 'test13',
     component: Layout,
-    meta: { title: '测试13', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试13', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test14',
     name: 'test14',
     component: Layout,
-    meta: { title: '测试14', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试14', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test15',
     name: 'test15',
     component: Layout,
-    meta: { title: '测试15', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试15', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test16',
     name: 'test16',
     component: Layout,
-    meta: { title: '测试16', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试16', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test17',
     name: 'test17',
     component: Layout,
-    meta: { title: '测试17', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试17', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test18',
     name: 'test18',
     component: Layout,
-    meta: { title: '测试18', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试18', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test19',
     name: 'test19',
     component: Layout,
-    meta: { title: '测试19', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试19', icon: 'fa fa-windows', cache: true, menu: true }
 }, {
     path: '/test20',
     name: 'test20',
     component: Layout,
-    meta: { title: '测试20', icon: 'el-icon-success', cache: true, menu: true }
+    meta: { title: '测试20', icon: 'fa fa-windows', cache: true, menu: true }
 }]
